339e01c827eb59b3a5bdd297fc64caf7e85232ad
Siphalor <info@siphalor.de>
2021-12-09 10:31:34 +0100

    Day 9: (Part 2)

--- 09/main.lua
+++ 09/main.lua
@@ -8,6 +8,9 @@ function main()
 	if (arg[1] == "part01")
 	then
 		xpcall(part01, error_handler)
+	elseif (arg[1] == "part02")
+	then
+		xpcall(part02, error_handler)
 	end
 end
 
@@ -15,9 +18,19 @@ function error_handler(err)
 	print("execution failed with: ", err)
 end
 
+function read_data()
+	local i = 1
+	local data = {}
+	for value in io.lines(arg[2])
+	do
+		data[i] = value
+		i = i + 1
+	end
+	return data
+end
+
 function find_minima(data)
-	local minima
-	minima = {}
+	local minima = {}
 	for y = 1,#data,1
 	do
 		for x = 1,#data[y],1
@@ -52,15 +65,7 @@ function find_minima(data)
 end
 
 function part01()
-	local data, risk
-
-	i = 1
-	data = {}
-	for value in io.lines(arg[2])
-	do
-		data[i] = value
-		i = i + 1
-	end
+	local data = read_data()
 
 	risk = 0
 	local minima = find_minima(data)
@@ -72,8 +77,85 @@ function part01()
 	print(risk)
 end
 
+function part02()
+	local data = read_data()
+	local basin_data = {}
+	for y = 1,#data,1
+	do
+		basin_data[y] = {}
+		for x = 1,#data[y],1
+		do
+			basin_data[y][x] = char_at(data[y], x) + 0
+		end
+	end
+	local basins = {}
+
+	local minima = find_minima(data)
+	for i, m in ipairs(minima)
+	do
+		basins[i] = 1
+		basin_data[m[1]][m[2]] = -i
+	end
+
+	local current
+	local changed = true
+	while changed
+	do
+		changed = false
+		for y = 1,#basin_data,1
+		do
+			for x = 1,#basin_data[y],1
+			do
+				current = basin_data[y][x]
+				if current >= 0
+				then
+					goto continue
+				end
+
+				if y > 1 and basin_data[y-1][x] >= 0 and basin_data[y-1][x] < 9
+				then
+					basin_data[y-1][x] = current
+					basins[-current] = basins[-current] + 1
+					changed = true
+				end
+
+				if x > 1 and basin_data[y][x-1] >= 0 and basin_data[y][x-1] < 9
+				then
+					basin_data[y][x-1] = current
+					basins[-current] = basins[-current] + 1
+					changed = true
+				end
+
+				if y < #basin_data and basin_data[y+1][x] >= 0 and basin_data[y+1][x] < 9
+				then
+					basin_data[y+1][x] = current
+					basins[-current] = basins[-current] + 1
+					changed = true
+				end
+
+				if x < #basin_data[y] and basin_data[y][x+1] >= 0 and basin_data[y][x+1] < 9
+				then
+					basin_data[y][x+1] = current
+					basins[-current] = basins[-current] + 1
+					changed = true
+				end
+				::continue::
+			end
+		end
+	end
+
+	table.sort(basins, function(a, b)
+		return a > b
+	end)
+	print(basins[1] * basins[2] * basins[3])
+end
+
 function char_at(s, index)
 	return string.sub(s, index, index)
+end
+
+function replace_char_at(s, with, at)
+	return string.sub(s, 1, at - 1) .. with .. string.sub(s, at + 1)
 end
 
 main()
