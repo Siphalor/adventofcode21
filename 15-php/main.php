78a58bafbb9f48b1dcba9f23b964e9f58d98a77e
Siphalor <info@siphalor.de>
2021-12-15 12:32:32 +0100

    Day 15: PHP (Part 1)

---
+++ 15-php/main.php
@@ -0,0 +1,17 @@
+<?php
+
+if (count($argv) != 3) {
+    die('incorrect number of arguments');
+}
+
+$data = array_map(fn($line): array => array_map(fn($char): int => $char + 0, str_split(trim($line))), file($argv[2]));
+
+switch ($argv[1]) {
+case "part01":
+    include_once "part01.php";
+    break;
+case "part02":
+    break;
+}
+?>
+<?php /* vim: set expandtab tabstop=4 smarttab shiftwidth=4: */ ?>
