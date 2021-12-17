4a5c28509d55657db6d83fc6769fcc5f6658af2b
Siphalor <info@siphalor.de>
2021-12-17 22:50:28 +0100

    Day 17: Clojure (Part 2)

--- 17-clojure/main.clj
+++ 17-clojure/main.clj
@@ -17,40 +17,53 @@
                 (< vx 0) (+ vx 1)
                 (> vx 0) (- vx 1)
                 :else 0
-              )
+                )
               (- vy 1)))))
 
 (def ^:const DEBUG false)
 (def ^:const UNCHANGED_CNT 50)
 
 (def highest (atom [ -1 0 ]))
+(def allHits (atom []))
 (def fileContent (slurp (last *command-line-args*)))
-(let [ [ _ g1 g2 g3 g4 ]
-      (map read-string (re-find #"target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)\s*" fileContent))
-      ]
+
+(defn findHits [ vy0 dvy _tx1 _tx2 _ty1 _ty2 ]
   (let [ unchanged (atom UNCHANGED_CNT)
-         tx1 (min g1 g2)
-         tx2 (max g1 g2)
-         ty1 (max g3 g4)
-         ty2 (min g3 g4)
-         vy (atom ty1) ]
+        tx1 (min _tx1 _tx2)
+        tx2 (max _tx1 _tx2)
+        ty1 (max _ty1 _ty2)
+        ty2 (min _ty1 _ty2)
+        vy (atom vy0) ]
     (while (> @unchanged 0)
       (if DEBUG (println "vy:" @vy))
       (let [ vx (atom 0) ]
         (swap! vx (vector 0))
-        (while (<= @vx tx1)
+        (while (<= @vx tx2)
           (if (traceShot tx1 ty1 tx2 ty2 0 0 @vx @vy)
             (do
               (if DEBUG (println "found vx:" @vx "vy:" @vy))
-              (swap! highest (constantly [ @vx @vy ]))
+              (let [ pair [ @vx @vy ] ]
+                (swap! allHits (fn [prev] (conj prev pair)))
+                (swap! highest (constantly pair)))
               (swap! unchanged (constantly UNCHANGED_CNT))))
           (swap! vx inc))
-      (swap! vy inc))
+        (swap! vy (fn [prev] (+ prev dvy)))
 
-      (if (> @vy 0) (swap! unchanged dec))
-      (if DEBUG (println @unchanged "unchanged left")))
-    )
+        (swap! unchanged dec)
+        (if DEBUG (println @unchanged "unchanged left")))
+      )))
+
+(let [ [ _ g1 g2 g3 g4 ]
+      (map read-string (re-find #"target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)\s*" fileContent)) ]
+  (case (first *command-line-args*)
+    "part01" (do
+               (findHits g3 1 g1 g2 g3 g4)
+               (println "best pair:" @highest)
+               (println "largest height:" (* (/ (second @highest) 2) (+ (second @highest) 1))))
+    "part02" (let [ ymin (min g3 g4) ]
+               (findHits ymin -1 g1 g2 g3 g4)
+               (findHits (inc ymin) 1 g1 g2 g3 g4)
+               (if DEBUG (println @allHits))
+               (println "count of all matching velocities:" (count @allHits)))
+    (println "unknown subcommand"))
   )
-
-(println "best pair:" @highest)
-(println "largest height:" (* (/ (second @highest) 2) (+ (second @highest) 1)))
