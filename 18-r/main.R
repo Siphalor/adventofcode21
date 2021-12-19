9eed8589700bd9f52f06858997fab7ebb50f784c
Siphalor <info@siphalor.de>
2021-12-19 13:01:03 +0100

    Day 18: R (Part 2)

--- 18-r/main.R
+++ 18-r/main.R
@@ -8,7 +8,26 @@ if (length(args) != 2) {
 }
 
 sns <- map(readLines(args[2]), ~ snailfish.parse(.))
-res <- reduce(sns[-1], ~ snailfish.add(.x, .y), .init = sns[[1]])
 
-print(snailfish.toString(res))
-print(snailfish.magnitude(res))
+if (args[[1]] == "part01") {
+  res <- reduce(sns[-1], ~ snailfish.add(.x, .y), .init = sns[[1]])
+  print(snailfish.toString(res))
+  print(snailfish.magnitude(res))
+} else if (args[[1]] == "part02") {
+  max <- 0
+  for (i in seq_along(sns)) {
+    for (j in i:length(sns)) {
+      val <- snailfish.magnitude(snailfish.add(sns[[i]], sns[[j]]))
+      if (val > max) {
+        max <- val
+      }
+      val <- snailfish.magnitude(snailfish.add(sns[[j]], sns[[i]]))
+      if (val > max) {
+        max <- val
+      }
+    }
+  }
+  print(max)
+} else {
+  print("unknown subcommand")
+}
